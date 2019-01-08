f5ac9a0f7075908c1b36d4671e6384eeb445740b
jaceenet <xjc@xjc.dk>
2019-01-09 00:22:19 +0100

    gulp css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 const browsersync = require("browser-sync").create();
+inlineCss = require('gulp-inline-css');
 
 // BrowserSync
 function browserSync(done) {
@@ -15,21 +16,36 @@ function browserSync(done) {
 
 // BrowserSync Reload
 function browserSyncReload(done) {
+  html();
+
   browsersync.reload();
   done();
+}
+
+function html(){
+  return gulp.src('./*.html')
+        .pipe(inlineCss())
+        .pipe(gulp.dest('./output'));
 }
 
 // keeps gulp from crashing for scss errors
 function scss(){
   return gulp.src('./sass/*.scss')
       .pipe(sass({ errLogToConsole: true, outputStyle: "expanded" }))
-      .pipe(gulp.dest('./public/css'));
+      .pipe(gulp.dest('./output/css'));
 };
+
+function build() {
+  scss();
+  html();
+}
 
 function watchfiles() {  
   gulp.watch('./sass/**/*.scss', scss);
-  gulp.watch('./public/**/*', browserSyncReload);
+  gulp.watch('./html/**/*', html);
 }
 
 const watch = gulp.parallel(watchfiles, browserSync);
 exports.watch = watch;
+exports.html = html;
+exports.build = build;
