67a1f94554f890efc272f2bae9e28c107d1caf26
jaceenet <xjc@xjc.dk>
2019-01-10 22:42:58 +0100

    base reset

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
 var browsersync = require("browser-sync").create();
@@ -22,15 +23,12 @@ function browserSync(done) {
 
 // BrowserSync Reload
 function browserSyncReload(done) {
-  html();
-
   browsersync.reload();
   done();
 }
 
 function html(){
   return gulp.src('./src/*.html')
-        //.pipe(inlineCss())
         .pipe(gulp.dest(options.wwwroot));
 }
 
@@ -38,12 +36,19 @@ function html(){
 function scss(){
   return gulp.src('./src/*.scss')
       .pipe(sass({ errLogToConsole: true, outputStyle: "expanded" }))
+      .on('error', gutil.log)
       .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
         }))
+      
       .pipe(gulp.dest(options.wwwroot));
 };
+
+function scssWatched(done){
+  scss();
+  browserSyncReload(done);
+}
 
 function build() {
   scss();
@@ -51,8 +56,8 @@ function build() {
 }
 
 function watchfiles() {  
-  gulp.watch('./sass/**/*.scss', scss);
-  gulp.watch('./html/**/*', html);
+  gulp.watch('./src/**/*.scss', scssWatched);
+  gulp.watch('./src/**/*.html', html);
 }
 
 const watch = gulp.parallel(build, watchfiles, browserSync);
